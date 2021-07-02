41babb7046220081a659c8d5a128ef10da891125
Gordon G <gordon@expressvpn.com>
2021-07-02 11:04:13 +0800

    add kubernetes-cli v1.18.9 and kustomize v3.5.5

---
+++ kubernetes-cli@1.18.9.rb
@@ -0,0 +1,45 @@
+class KubernetesCliAT1189 < Formula
+  desc "Kubernetes command-line interface"
+  homepage "https://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      :tag      => "v1.18.9",
+      :revision => "94f372e501c973a7fa9eb40ec9ebd2fe7ca69848"
+  head "https://github.com/kubernetes/kubernetes.git"
+
+  depends_on "go" => :build
+
+    def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    # Make binary
+    system "make", "WHAT=cmd/kubectl"
+    bin.install "_output/bin/kubectl"
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "bash")
+    (bash_completion/"kubectl").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "zsh")
+    (zsh_completion/"_kubectl").write output
+
+    # Install man pages
+    # Leave this step for the end as this dirties the git tree
+    system "hack/generate-docs.sh"
+    man1.install Dir["docs/man/man1/*.1"]
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource)
+                         .instance_variable_get(:@specs)[:revision],
+                   version_output
+    end
+  end
+end
---
+++ kustomize@3.5.5.rb
@@ -0,0 +1,53 @@
+class KustomizeAT355 < Formula
+  desc "Template-free customization of Kubernetes YAML manifests"
+  homepage "https://github.com/kubernetes-sigs/kustomize"
+  url "https://github.com/kubernetes-sigs/kustomize.git",
+      :tag      => "kustomize/v3.5.5",
+      :revision => "897e7b6e61e65188d846c32bd3af9ef68b0f746a"
+  head "https://github.com/kubernetes-sigs/kustomize.git"
+
+  depends_on "go" => :build
+
+  def install
+    revision = Utils.popen_read("git", "rev-parse", "HEAD").strip
+
+    cd "kustomize" do
+      ldflags = %W[
+        -s -X sigs.k8s.io/kustomize/api/provenance.version=kustomize/v#{version}
+        -X sigs.k8s.io/kustomize/api/provenance.gitCommit=#{revision}
+        -X sigs.k8s.io/kustomize/api/provenance.buildDate=#{Time.now.iso8601}
+      ]
+      system "go", "build", "-ldflags", ldflags.join(" "), "-o", bin/"kustomize"
+    end
+  end
+
+  test do
+    assert_match "kustomize/v#{version}", shell_output("#{bin}/kustomize version")
+
+    (testpath/"kustomization.yaml").write <<~EOS
+      resources:
+      - service.yaml
+      patchesStrategicMerge:
+      - patch.yaml
+    EOS
+    (testpath/"patch.yaml").write <<~EOS
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        selector:
+          app: foo
+    EOS
+    (testpath/"service.yaml").write <<~EOS
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        type: LoadBalancer
+    EOS
+    output = shell_output("#{bin}/kustomize build #{testpath}")
+    assert_match /type:\s+"?LoadBalancer"?/, output
+  end
+end
